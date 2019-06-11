8484d6ed698ffec6c5ff0b4dfe4ff9604f7064f7
imperative-robot-4943 <drccamp32@gmail.com>
2019-06-11 23:07:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function happyHolidaysTo(name){
    return `Happy holidays, ${name}!`
 } 
 
-function happyHolidayTO(string){
+function happyHolidayTO(string, string){
    return `Happy ${string}, ${string}!`
 }
 
